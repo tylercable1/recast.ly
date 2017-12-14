6c58c2049e6855d48f651d3cd260e713e0abd624
tylercable1 <tylercable7@gmail.com>
2017-12-13 19:22:55 -0800

    Render App view with video list, player & search

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,11 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+
+//props.videos = video list array
+
+var VideoList = (props) => (
+ <div className="video-list">
+    {props.videos.map((video) =>
+      <VideoListEntry video={video} />
+    )}
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 // TODO: Render the `App` component to the DOM
 
+ReactDOM.render(<App videos={window.exampleVideoData}/>, document.getElementById('app'));
